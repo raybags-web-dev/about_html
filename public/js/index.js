4eba5d0addcbbf6e938fbece47254d28300c1f99
raybags <baguma.github@gmail.com>
2021-05-21 01:24:25 +0200

    init commit

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {
+  console.log("jquery loaded");
+
+
+  // functions running on window resize
+  $(window).on("resize", () => {});
+  $(window).on("orientationChange", () => {});
+  $(window).on("load", () => {});
+  $(window).on("mousemove", () => {});
+});
