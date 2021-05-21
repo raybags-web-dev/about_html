eb395709ee6f874b56f8606e1144c3c0fc989847
raybags <baguma.github@gmail.com>
2021-05-22 01:16:33 +0200

    implimenting scrollspy

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,23 @@
 $(document).ready(function () {
   console.log("jquery loaded");
 
+  // active class switcher on scroll (Scroll spy)
+  $(window).on("scroll", function () {
+    let scrollBarLocation = $(this).scrollTop();
+    let scrollLinks = $(".hyper-link");
+
+    scrollLinks.each(function () {
+      let sectionOffset = $(this.hash).offset().top - 70;
+      if (sectionOffset <= scrollBarLocation) {
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+      }
+    });
+  });
 
   // functions running on window resize
-  $(window).on("resize", () => {});
-  $(window).on("orientationChange", () => {});
-  $(window).on("load", () => {});
-  $(window).on("mousemove", () => {});
+  // $(window).on("resize", () => {});
+  // $(window).on("orientationChange", () => {});
+  // $(window).on("load", () => {});
+  // $(window).on("mousemove", () => {});
 });
